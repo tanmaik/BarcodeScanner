7bd9c116e18c91b1e6f426921d7d53c1440c2417
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2021-09-06 23:47:12 -0400

    finish BarcodeScanner application

--- BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -8,10 +8,14 @@
 import SwiftUI
 
 struct BarcodeScannerView: View {
+    
+    @StateObject var viewModel = BarcodeScannerViewModel()
+    
     var body: some View {
         NavigationView {
             VStack {
-                Rectangle()
+                ScannerView(scannedCode: $viewModel.scannedCode,
+                            alertItem: $viewModel.alertItem)
                     .frame(maxWidth: .infinity, maxHeight: 300)
                 
                 Spacer()
@@ -20,13 +24,19 @@ struct BarcodeScannerView: View {
                 Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
                     .font(.title)
                 
-                Text("Not Yet Scanned")
+                Text(viewModel.statusText)
                     .bold()
                     .font(.largeTitle)
-                    .foregroundColor(.green)
+                    .foregroundColor(viewModel.statusTextColor)
                     .padding()
+                
             }
             .navigationTitle("BarcodeScanner")
+            .alert(item: $viewModel.alertItem) { alertItem in
+                Alert(title: Text(alertItem.title),
+                      message: Text(alertItem.message),
+                      dismissButton: alertItem.dismissButton)
+            }
         }
     }
 }
---
+++ BarcodeScanner/Utilities/Alert.swift
@@ -0,0 +1,26 @@
+//
+//  Alert.swift
+//  BarcodeScanner
+//
+//  Created by Tanmai Kalisipudi on 9/6/21.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    
+    let id = UUID()
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
+                                              message: "Something is wrong with the camera. Unable to capture the input.",
+                                              dismissButton: .default(Text("OK")))
+    
+    static let invalidScannedType = AlertItem(title: "Invalid Scan Type",
+                                              message: "The value scanned is not valid. This app scans EAN-8 and EAN-13.",
+                                              dismissButton: .default(Text("OK")))
+}
