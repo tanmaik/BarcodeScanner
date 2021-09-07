3000e9b3cfe09b448846b57818be317ca14e7b65
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2021-09-06 22:05:48 -0400

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Tanmai Kalisipudi on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
